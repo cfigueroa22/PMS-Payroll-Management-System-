d11957721083a9a2b7b4ab429fa7db81ade5b75c
Carlos Figueroa <carfig7186@students.ecpi.edu>
2022-12-02 14:41:35 -0500

    employee class created

--- Payroll/Employee.cs
+++ Payroll/Employee.cs
@@ -5,3 +5,27 @@ Assignment: CIS317 Payroll Management System
 
 Description:
 */
+public class Employee 
+{
+    public int EmployeeId { get; set; }
+    public string EmployeeName { get; set; }
+    public string Address { get; set; }
+    public string PhoneNumber { get; set; }
+    public double HourlyRate { get; set; }
+
+    public Employee(int id, string employeeName, string address, string phoneNum, double rate)
+    {
+        EmployeeId = id;
+        EmployeeName = employeeName;
+        Address = address;
+        PhoneNumber = phoneNum;
+        HourlyRate = rate; 
+    }
+
+    public override string ToString()
+    {
+        return string.Format("\nEmployeed ID: {0}\nEmployee Name: {1}\nAddress: {2}\n
+        Phone Number: {3}\nHour Rate: {4}", EmployeeId, EmployeeName, Address, PhoneNumber,
+        HourlyRate);
+    }
+}
