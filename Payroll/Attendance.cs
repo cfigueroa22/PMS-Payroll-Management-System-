073c244810457fbb660b0f2e740434b3b6434d22
Carlos Figueroa <carfig7186@students.ecpi.edu>
2022-12-02 15:35:42 -0500

    attendance class created

---
+++ Payroll/Attendance.cs
@@ -0,0 +1,28 @@
+/*
+Name: Carlos Figueroa
+Date: December 1, 2022
+Assignment: CIS317 Payroll Management System 
+
+Description:
+*/
+public class Attendance : Employee
+{
+    public double HoursWork { get; set; }
+    public int DaysWork { get; set; }
+    public bool OverTime { get; set; }
+
+    public Attendance(int id, string employeeName, string address, string phoneNum, double rate, double hoursWork, int daysWork, bool overTime) : base(id, employeeName, address, phoneNum,rate)
+    {
+        HoursWork = hoursWork;
+        DaysWork = daysWork;
+        OverTime = overTime;
+    }
+
+    public override string ToString()
+    {
+        return string.Format(
+            "{0}Hours Worked: {1:F1}\nDays Worked: {2}\nOvertime: {3}",
+            base.ToString(), HoursWork, DaysWork, OverTime);
+    }
+
+}
